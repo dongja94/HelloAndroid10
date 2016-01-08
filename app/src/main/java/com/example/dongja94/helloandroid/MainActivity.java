d400d2fb44f65be304cc52d1f8dbd25382288915
dongja94 <dongja94@gmail.com>
2016-01-08 17:47:13 +0900

    id

--- app/src/main/java/com/example/dongja94/helloandroid/MainActivity.java
+++ app/src/main/java/com/example/dongja94/helloandroid/MainActivity.java
@@ -1,13 +1,38 @@
 package com.example.dongja94.helloandroid;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    int count;
+
+    TextView messageView;
+    EditText inputView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        messageView = (TextView)findViewById(R.id.text_message);
+        inputView = (EditText)findViewById(R.id.edit_input);
+
+        Button btn = (Button)findViewById(R.id.btn_send);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                messageView.setText(inputView.getText().toString());
+            }
+        });
+
     }
+
 }
